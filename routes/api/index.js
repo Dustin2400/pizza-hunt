41a0fbbaa295bd212dca944b225496ad6edac3ea
Dustin Grijalva <dustin2400@hotmail.com>
2022-01-13 00:48:27 -0600

    made comment functionality

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
-
+const commentRoutes = require('./comment-routes')
 // add prefix of `pizzas` to routes created in `pizza-routes.js`
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
